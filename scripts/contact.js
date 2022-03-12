dbf886b3d3564968bc11c7ddfcab65b930e0da14
m2ran <101386399+m2ran@users.noreply.github.com>
2022-03-12 12:45:01 +0900

    Create contact.js

---
+++ scripts/contact.js
@@ -0,0 +1,39 @@
+window.contact = window.contact || {};
+
+window.contact.checkValidation = function(){
+    if(!$('input[id="name"]').val()){
+        $('input[id=submit]').attr('disabled', 'disabled');
+        return false;
+    }
+
+    $('input[id="submit"]').removeAttr('disabled');
+    return true;
+}
+
+window.contact.send = function(){
+    var name = $('input[id="name"]').val() 
+    data = {
+        name: name,
+    }
+    window.contact.ajax(data);
+}
+
+window.contact.ajax = function(data){
+    var url = 'https://script.google.com/macros/s/AKfycbya9nGdKpE7R0DllRA6xws8tNaEzsYg4bHhY3hhyYQxw-r6DxaokPJKulKNtbycYcko/exec'; // Change here: Your GAS URL here
+    $.ajax({
+        url: url,
+        type:'POST',
+        data: data
+    }).done(function(res){
+        if(res.response != "success") {
+            console.log(JSON.stringify(res.error));
+            alert('送信失敗'); 
+            return;
+        }
+        alert('送信完了');
+    }).fail(function(){
+        alert('送信失敗'); 
+    }).always(function(){
+        location.href="./index.html";
+    })
+}
